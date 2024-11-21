328f29d2a15251d550762f7d819a177f478be59b
AlWegner <alanwegnerm@gmail.com>
2024-11-21 02:04:52 -0300

    Creando funcion de mostrarCarrito

--- js/app.js
+++ js/app.js
@@ -12,6 +12,21 @@ const agregarCarrito = (e) => {
         cantidad: 1,
         precio: parseInt(e.target.dataset.precio),
     };
+
+    const posicion = carritoArray.findIndex((item) => item.titulo === producto.titulo);
+
+    if (posicion === -1) {
+        carritoArray.push(producto)
+    } else {
+        carritoArray[posicion].cantidad++;
+    };
+    mostrarCarrito();
+};
+
+const mostrarCarrito = () => {
+    carritoArray.forEach ((item) => {
+        console.log(item);
+    });
 };
 
 document.addEventListener('click', (e) => {
