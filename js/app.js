cdc0dcf8f1d21a41c65432e626142f994b802378
AlWegner <alanwegnerm@gmail.com>
2024-11-21 01:43:26 -0300

    Detectando botones y creando funcion para agregar productos al carrito

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ const template = document.querySelector('#template');
 const footer = document.querySelector('#footer');
 const templateFooter = document.querySelector('#templateFooter');
 
+const agregarCarrito = (e) => {
+    console.log(e);
+};
+
 document.addEventListener('click', (e) => {
-    console.log(e.target);
-})
+    if (e.target.matches('.btn-primary')) {
+        agregarCarrito(e);
+    };
+});
